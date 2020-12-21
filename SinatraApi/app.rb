987b10a2303b593639a01000b92d65f64323e4ef
Diego Fernandes <diego.fernandes@enjoei.com.br>
2020-12-21 18:55:41 -0300

    learning sinatra api - 2-class

--- SinatraApi/app.rb
+++ SinatraApi/app.rb
@@ -1,2 +1,26 @@
-require "httparty"
+require "sinatra"
+require "mongoid"
+# DB connect
 
+Mongoid.load! "mongoid.yml"
+
+class Book
+    include Mongoid::Document #import dos recursos do BD para esta classe 
+
+    field: :title, type: String
+    field: :author, type: String
+    field: :isbn, type: String
+
+end
+
+get "/" do
+
+    content_type: "application/json"
+    { message:"Welcome to Book api!"}.to_json
+end 
+
+get "/books" do
+    content_type: "application/json"
+    return Book.all.to_json #busca por todo doc da classe
+
+end
